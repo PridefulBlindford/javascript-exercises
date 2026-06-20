214a6c88c0bcdeb14e3f0438fbc0975a3909f005
Nathanael Ogden <104324138+PridefulBlindford@users.noreply.github.com.>
2026-06-20 08:30:03 -0400

    edits for 06 assignmenet
    changed the script file so that all tests passed for the 06 exercise.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(message,timesToRepeat) {
+    let finalString="";
+    if(timesToRepeat<0){
+        return "ERROR";
+    }
+for(let i=1;i<=timesToRepeat;i++){
+    
+    finalString+=message;
+}
+return finalString;
 };
 
 // Do not edit below this line
