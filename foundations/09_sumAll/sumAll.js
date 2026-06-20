f1a2adf2e4e1863e1604db074718cbfa50b549a1
Nathanael Ogden <104324138+PridefulBlindford@users.noreply.github.com.>
2026-06-20 13:55:17 -0400

    added the edits for 09 exercise
    changed the 09 file so that all the tests worked.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(firstNumber,secondNumber) {
+    let smallerNumber,sum=0,largerNumber;
+    if(firstNumber>secondNumber){
+        smallerNumber=secondNumber;
+        largerNumber=firstNumber;
+    }
+    else if(firstNumber <0 || secondNumber<0){
+        return "ERROR";
+    }
+    else if(firstNumber%1!==0||secondNumber%1!==0){
+        return "ERROR";
+    }
+    else if(typeof firstNumber!=="number"||typeof secondNumber!=="number"){
+        return "ERROR";
+    }
+    else{
+        smallerNumber=firstNumber;
+        largerNumber=secondNumber;
+    }
+    for(let i=smallerNumber;i<=largerNumber;i++){
+        sum+=i;
+
+    }
+
+    return sum;
 
 };
 
