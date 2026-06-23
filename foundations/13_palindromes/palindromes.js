8c11aec867442b57ae8989030e0ef7b6fa3c259b
Nathanael Ogden <104324138+PridefulBlindford@users.noreply.github.com.>
2026-06-23 16:48:30 -0400

    the 13 example exercise
    editing the javascript file to ensure all specs worked, including using array functions and reg ex.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (message) {
+    let cleanMessage=message.toLowerCase().replace(/[^a-z0-9]/g,"");
+    let reversedMessage=cleanMessage.split("").reverse().join("");
+    if(cleanMessage===reversedMessage){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
