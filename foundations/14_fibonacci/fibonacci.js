0c37f863133d0f3f33ec1928cc9bf10a5fc0eeab
Nathanael Ogden <104324138+PridefulBlindford@users.noreply.github.com.>
2026-06-23 17:31:18 -0400

    the 14 exercise
    edited the 14 exercise file so that it mmatched the spec file.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let total=0,firstPrevious=0,secondPrevious=0;
+    if(number===0){
+        return 0;
+    }
+    else if(number<0){
+        return "OOPS";
+    }
+    for(let i=1;i<=number;i++){
+        if(i===1){
+            total=1;
+            firstPrevious=1;
+        }
+        else if(i===2){
+            secondPrevious=1;
+        }
+        
+        else{
+            total=firstPrevious+secondPrevious;
+            secondPrevious=firstPrevious;
+            firstPrevious=total;
+        }
+    }
+    return total;
 
 };
 
